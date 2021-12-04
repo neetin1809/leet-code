cff146cc1d1c598913e72d7acdc49ab3e596ee2a
Nitin Vaswani <nitin.vaswani@rediffmail.com>
2021-12-04 20:38:58 +0530

    Added code for first missing positive number

---
+++ LeetCode/BalancedParenthesis/FirstMissingPositive.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedParenthesis
+{
+    //https://leetcode.com/problems/first-missing-positive/
+    public class FirstMissingPositiveInCollection
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            //3 4 -1 1
+            int missing = 1;
+
+            if (!nums.Any(x => x == missing))
+                return missing;
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] <= 0)
+                    continue;
+                if (nums[i] == missing)
+                {
+                    missing++;
+                }
+                else if(nums[i] == missing + 1)
+                {
+                    return missing;
+                }
+            }
+            return missing;
+        }
+    }
+    }
+}
